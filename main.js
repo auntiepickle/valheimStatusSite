5f70a61ac0a3b87c69f295c1ef408cfe111c9e65
Piekel <chgross@outlook.com>
2023-04-07 15:27:03 -0700

    collapsibles

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ async function getJSONData() {
   }
 
   async function init(){
+   createCollapsible();
    await getJSONData();
   }
 
@@ -33,6 +34,23 @@ function updateHTMLStatus(){
 
 function elementbyIdUpdate(el, html){
     document.getElementById(el).innerHTML = html;
+}
+
+function createCollapsible(){
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+        content.style.display = "none";
+        } else {
+        content.style.display = "block";
+        }
+    });
+    }
 }
 
 init();
